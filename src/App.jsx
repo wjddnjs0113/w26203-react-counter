3f9f3bb818916acda7ff9ec9f8bce2312cb5bf1e
Jung won <wjddnjs2975@gmail.com>
2026-09-17 18:25:05 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  return (
+    <>
+      <h1>Count:{count}</h1>
+      <button 
+      onClick={ () => {
+        setCount(prev => prev + 1)
+        setCount(prev => prev + 1)
+        setCount(prev => prev + 1)
+        setCount(prev => prev + 1)
+        setCount(prev => prev + 1)
+      } }
+      >
+        증가
+      </button>
+    </>
+  )
+}
+
+export default App

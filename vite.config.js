2b394bacc51a090a99f3cce8d54a35fc22221556
Jung won <wjddnjs2975@gmail.com>
2026-09-20 22:47:50 +0900

    :pencil2: 경로 위치 재설정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/w26w03-react-counter/',
+  base: '/w26203-react-counter/',
 })
